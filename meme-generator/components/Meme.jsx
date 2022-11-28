cbf5ebaa3555493a3da3c557597eb889cc28a155
malachi3keys <bhibbler.pltw@gmail.com>
2022-11-28 16:01:06 -0500

    update useState for text input

--- meme-generator/components/Meme.jsx
+++ meme-generator/components/Meme.jsx
@@ -3,30 +3,50 @@ import { useState } from 'react'
 
 export default function Meme() {
     const [topMemes, setTopMemes] = useState(memesData.data.memes); 
+
     const [randomMeme, setRandomMeme] = useState({
-        memeImg: topMemes[0],
+        memeImg: topMemes[2],
         topText: '',
         bottomText: ''
     });
 
-    function newMeme() {
+    function newMeme(event) {
+        event.preventDefault()
         var num = Math.floor(Math.random() * topMemes.length);
         setRandomMeme(prevMeme => ({...prevMeme, memeImg: topMemes[num]}));
     }
 
-    
+    function changeText(event) {
+        const {name, value} = event.target;
+        setRandomMeme(prevText => ({...prevText, [name]: value}));
+    }
+
     return (
         <main>
-            <div className='caption'>
-                <input className='textbox' placeholder='Top text' type='text' />
-                <input className='textbox' placeholder='Bottom text' type='text' />
+            <form className='caption'>
+                <input 
+                    name='topText' 
+                    className='textbox' 
+                    placeholder='Top text' 
+                    type='text'
+                    value={randomMeme.topText}
+                    onChange={changeText}
+                />
+                <input 
+                    name='bottomText' 
+                    className='textbox' 
+                    placeholder='Bottom text' 
+                    type='text' 
+                    value={randomMeme.bottomText}
+                    onChange={changeText}
+                />
                 <button className='get-image' onClick={newMeme}>Get a new meme image 🖼️</button>
-            </div>
+            </form>
             
             <div className='meme'>
+                {randomMeme && <img src={randomMeme.memeImg.url} className='meme-image'/>}
                 <p className='top-caption'>{randomMeme.topText}</p>
                 <p className='bottom-caption'>{randomMeme.bottomText}</p>
-                {randomMeme && <img src={randomMeme.memeImg.url} className='meme-image'/>}
             </div>
         </main>    
     )
