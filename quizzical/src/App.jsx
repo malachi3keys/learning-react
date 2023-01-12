fce0ba8386a1f5e1e1d4b9e1950fb1312298821c
malachi3keys <bhibbler.pltw@gmail.com>
2023-01-12 16:54:35 -0500

    display questions & update CSS

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -2,41 +2,28 @@ import { useState, useEffect } from 'react'
 import  Intro from './components/Intro'
 import  Quiz from './components/Quiz'
 import  Question from './components/Question'
+import { nanoid } from 'nanoid'
 import './App.css'
 
 export default function App() {
   const [showQ, setShowQ] = useState(false)
   const [questions, setQuestions] = useState([])
-  var test
-
 
   function startQuiz() {
     setShowQ(true)
-    // console.log(questions)
   }
-
-  //pull 5 questions from api and map to set props for each one with horizontal rule under each one
-  // also need a check answers button
-  //<button className='check-answers'>Check answers</button> 
 
   useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=5&category=9&type=multiple')
         .then((res) => res.json())
         .then((data) => setQuestions(data.results))
-
-    test = newQuestions()
   },[showQ])
 
 
-function newQuestions() {
-  var id = 0
   const quiz = questions.map((q) => {
-    id = id + 1;
     var randomAnswers = [q.correct_answer]
     randomAnswers.push(...q.incorrect_answers) //add incorrect answers to array
 
-    //number of possible answers for multiple choice or true/false question
-    var length = q.type == 'multiple' ? 4 : 2 
     //randomize answers
     for (var i = randomAnswers.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
@@ -44,30 +31,26 @@ function newQuestions() {
       randomAnswers[i] = randomAnswers[j];
       randomAnswers[j] = temp;
     }
-    // console.log(randomAnswers)
 
     return (
       <Question 
-        key={id}
+        key={nanoid()}
         question ={q.question}
         answers = {randomAnswers}
         correct = {q.correct_answer}
-        // id={q.id}
-        // value={q.value}
-        // isHeld={q.isHeld}
-        // handleHold={holdNum}
+        anum = {randomAnswers.length}
       />
     )
   })
-
-  return quiz
-}  
 
 
   return (
     <main className='App'>
       {showQ 
-      ?{test}
+      ?<div>
+        {quiz}
+        <button className='check-answers'>Check answers</button>
+      </div>
       :<Intro 
         startQuiz = {startQuiz}
       />
--- quizzical/src/components/Question.jsx
+++ quizzical/src/components/Question.jsx
@@ -1,13 +1,34 @@
-export default function Question(props) {
+export default function Question(props) {    
+    if (props.anum == 4){
+        var mult = true
+    } else {
+        var mult = false
+    }
+
+    console.log(props.question)
+
     return (
         <div>
-            <h2>question{props.question}</h2>
-            <div className='answer-div'>
-                <button className={`answer-btn ${props.isSelected ? 'selected' : ''} ${props.isCorrect ? 'correct' : 'incorrect'}`}>answer1 {props.answers[0]}</button>
-                <button className={`answer-btn ${props.isSelected ? 'selected' : ''} ${props.isCorrect ? 'correct' : 'incorrect'}`}>answer2 {props.answers[1]}</button>
-                <button className={`answer-btn ${props.isSelected ? 'selected' : ''} ${props.isCorrect ? 'correct' : 'incorrect'}`}>answer3 {props.answers[2]}</button>
-                <button className={`answer-btn ${props.isSelected ? 'selected' : ''} ${props.isCorrect ? 'correct' : 'incorrect'}`}>answer4 {props.answers[3]}</button>
+            <div  className='question'>
+                <h2 className='question-title'>{props.question}</h2>
+                {mult &&
+                    <div className='answer-div'>
+                        <button className={`answer-btn ${props.isSelected ? 'selected' : ''} ${props.isCorrect ? 'correct' : 'incorrect'}`}>{props.answers[0]}</button>
+                        <button className={`answer-btn ${props.isSelected ? 'selected' : ''} ${props.isCorrect ? 'correct' : 'incorrect'}`}>{props.answers[1]}</button>
+                        <button className={`answer-btn ${props.isSelected ? 'selected' : ''} ${props.isCorrect ? 'correct' : 'incorrect'}`}>{props.answers[2]}</button>
+                        <button className={`answer-btn ${props.isSelected ? 'selected' : ''} ${props.isCorrect ? 'correct' : 'incorrect'}`}>{props.answers[3]}</button>
+                    </div>
+                } 
+                {mult || 
+                    <div className='answer-div'>
+                        <button className={`answer-btn ${props.isSelected ? 'selected' : ''} ${props.isCorrect ? 'correct' : 'incorrect'}`}>{props.answers[0]}</button>
+                        <button className={`answer-btn ${props.isSelected ? 'selected' : ''} ${props.isCorrect ? 'correct' : 'incorrect'}`}>{props.answers[1]}</button>
+                    </div>
+                } 
             </div>
+            <hr className='borderline'></hr>    
         </div>
+        
+       
     )
 }
